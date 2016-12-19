92c08d89e7ecc2994a30f53704595935261cd7a2
colorworld <563880188@qq.com>
2016-12-19 21:05:10 +0800

    简历初步

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,20 @@
 /*
-This is empty on purpose! Your code to build the resume will go here.
+ This is empty on purpose! Your code to build the resume will go here.
  */
+var email = "like apple";
+var newemail = email.replace("apple", "google");
+console.log(email);
+console.log(newemail)
+
+var awesoneThough = "My name is Cameron and I am AWESOME";
+var funThought = awesoneThough.replace("AWESOME", "FUN");
+// $("#main").append(funThought);
+
+
+var formattendName = HTMLheaderName.replace("%data%", "liwei");
+// $("#header").append(formattendName);
+
+var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattendName);
